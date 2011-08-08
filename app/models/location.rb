02419f5eb7f186b3badc0a9e9deccc1f596fb6ae
nankaist <nankaist@hotmail.com>
2011-08-08 11:10:45 -0400

    reverse geocode

--- app/models/location.rb
+++ app/models/location.rb
@@ -4,11 +4,11 @@ class Location < ActiveRecord::Base
   # geocoded_by :address
   # after_validation :geocode
   
-  # reverse_geocoded_by :latitude, :longitude
-  # after_validation :reverse_geocode
+  reverse_geocoded_by :latitude, :longitude
+  after_validation :reverse_geocode
   
-  geocoded_by :ip_address,
-    :latitude => :lat, :longitude => :lon
-  after_validation :geocode
+  # geocoded_by :ip_address,
+  #   :latitude => :lat, :longitude => :lon
+  # after_validation :geocode
 
 end
